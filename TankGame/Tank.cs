a1df3864a2af6117b13475af78ad9e35174704e4
unknown <a.akshabaev@c441a-10.kbtu.kz>
2017-04-03 10:22:04 +0600

    added Tank class

---
+++ TankGame/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace TankGame
+{
+    class Tank
+    {
+        public Point location;
+
+        public void DrawUp(Graphics g)
+        {
+
+        }
+
+        public void DrawDown(Graphics g)
+        {
+
+        }
+
+        public void DrawLeft(Graphics g)
+        {
+
+        }
+
+        public void DrawRight(Graphics g)
+        {
+
+        }
+
+        public void move(int dx, int dy)
+        {
+
+        }
+    }
+}
