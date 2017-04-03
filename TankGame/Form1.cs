85186fd45f9063a6b5953056af664d3060fd47b2
Nurzhigit <nusmailov@gmail.com>
2017-04-03 11:47:22 +0600

    added bullet

--- TankGame/Form1.cs
+++ TankGame/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace TankGame
 {
     public partial class Form1 : Form
     {
+        tank Tanks = new tank();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Tanks.DrawLeft(e.Graphics);
+            Tanks.bullet(e.Graphics);
+          //  Tanks.bulletright(e.Graphics);
+            Tanks.bulletdown(e.Graphics);
         }
     }
 }
--- TankGame/Tank.cs
+++ TankGame/Tank.cs
@@ -5,19 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
+using System.Threading;
 
 namespace TankGame
 {
-    class Tank
+    class tank
     {
         public Point location;
         public int length;
-        public int radius;
+        public int radius = 15;
         public int gunW, gunH;
+        Pen pen = new Pen(Color.Black);
+        public int x, y;
+        
 
         public void DrawUp(Graphics g)
         {
-
+            x = 20;
+            y = 20;
+            length = 40;
+            location.X = 30;
+            location.Y = 30;
+            pen.Color = Color.Black;
+            g.DrawEllipse(pen, location.X, location.Y , length, length);
+            g.DrawRectangle(pen, location.X, location.Y, length ,length);
+            g.DrawRectangle(pen, location.X + 10, location.Y - 20, length / 2, length);
         }
 
         public void DrawDown(Graphics g)
@@ -27,7 +40,16 @@ namespace TankGame
 
         public void DrawLeft(Graphics g)
         {
-
+            x = 20;
+            y = 20;
+            length = 40;
+            location.X = 30;
+            location.Y = 30;
+            pen.Color = Color.Black;
+            g.DrawEllipse(pen, location.X, location.Y, length, length);
+            g.DrawRectangle(pen, location.X, location.Y, length, length);
+            //g.DrawRectangle(pen, location.X + 10, location.Y - 20, length / 2, length);
+            g.DrawRectangle(pen, location.X-20, location.Y+10,length,length/2);
         }
 
         public void DrawRight(Graphics g)
@@ -37,7 +59,101 @@ namespace TankGame
 
         public void move(int dx, int dy)
         {
+           
+        }
+        public void bulletright(Graphics g)
+        {
+            //g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius, radius);
+            while (true)
+            {
 
+                g.Clear(Color.White);
+                g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius, radius);
+
+                location.X++;
+                Thread.Sleep(10);
+            }
+            
+        }
+        public void bulletleft(Graphics g)
+        {
+            //g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius, radius);
+            while (true)
+            {
+
+                g.Clear(Color.White);
+                g.DrawEllipse(pen, location.X + 150, location.Y + 10, radius, radius);
+                location.X--;
+                if (location.X == 0)
+                {
+                    break;
+                }
+                Thread.Sleep(10);
+            }
+        }
+        public void bulletup(Graphics g)
+        {
+            //g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius, radius);
+            while (true)
+            {
+                g.Clear(Color.White);
+                g.DrawEllipse(pen, location.X + 100, location.Y + 10, radius, radius);
+                location.Y--;
+                Thread.Sleep(10);
+            }
+        }
+        public void bulletdown(Graphics g)
+        {
+            //g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius, radius);
+            while (true)
+            {
+                g.Clear(Color.White);
+                g.DrawEllipse(pen, location.X + 100, location.Y + 10, radius, radius);
+                location.Y++;
+                Thread.Sleep(10);
+            }
+        }
+        public int d;
+        public void Movebullet(Graphics g)
+        {
+            switch (d){ 
+                case 1:
+                while (true)//bullet right
+                {
+                    g.Clear(Color.White);
+                    g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius, radius);
+                    location.X++;
+                    Thread.Sleep(10);
+                }
+                case 2:
+                while (true)//left
+                {
+                    g.Clear(Color.White);
+                    g.DrawEllipse(pen, location.X + 150, location.Y + 10, radius, radius);
+                    location.X--;
+                    Thread.Sleep(10);
+                }
+                case 3:
+                while (true)//up
+                {
+                    g.Clear(Color.White);
+                    g.DrawEllipse(pen, location.X + 150, location.Y + 10, radius, radius);
+                    location.Y--;
+                    Thread.Sleep(10);
+                }
+                case 4:
+                while (true)//down
+                {
+                    g.Clear(Color.White);
+                    g.DrawEllipse(pen, location.X + 150, location.Y + 10, radius, radius);
+                    location.Y--;
+                    Thread.Sleep(10);
+                }
+            }
+        }
+        public void bullet(Graphics g)
+        {
+            g.DrawEllipse(pen, location.X + 10, location.Y + 10, radius,radius);
         }
     }
 }
