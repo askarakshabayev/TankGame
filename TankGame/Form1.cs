02d8718f1d78d7bb8d3ab0b3ae5dd23a12cc2ad0
Elibay <elibay98@gmail.com>
2017-04-03 11:10:51 +0600

    DrawUp

--- TankGame/Form1.cs
+++ TankGame/Form1.cs
@@ -12,9 +12,16 @@ namespace TankGame
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Tank tank = new Tank(200, 200);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            tank.DrawUp(e.Graphics);
         }
     }
 }
--- TankGame/Tank.cs
+++ TankGame/Tank.cs
@@ -14,10 +14,23 @@ namespace TankGame
         public int length;
         public int radius;
         public int gunW, gunH;
-
+        public Tank (int x, int y)
+        {
+            length = 200;
+            radius = 150;
+            location.X = x;
+            location.Y = y;
+            gunW = 30;
+            gunH = 150;
+        }
         public void DrawUp(Graphics g)
         {
-
+            Brush a = new SolidBrush (Color.Red);
+            Brush b = new SolidBrush(Color.Green);
+            Brush c = new SolidBrush(Color.Black);
+            g.FillRectangle(b, location.X - length / 2, location.Y - length / 2, length, length);
+            g.FillEllipse(a, location.X - radius / 2, location.Y - radius / 2, radius, radius);
+            g.FillRectangle(c, location.X - gunW / 2, location.Y - gunH, gunW, gunH);
         }
 
         public void DrawDown(Graphics g)
